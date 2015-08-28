3f3e46a2ee7d73dd0d96ecf7888521883c228aea
spdjudd <spdjudd@yahoo.co.uk>
2015-08-28 13:59:51 +0100

    Use Common.Logging instead of direct log4net dependency

--- src/dotnet/SolutionVersion.cs
+++ src/dotnet/SolutionVersion.cs
@@ -2,13 +2,13 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyDescription("ZooKeeper Client for .NET http://zookeeper.apache.org")]
+[assembly: AssemblyDescription("ZooKeeper Client for .NET http://zookeeper.apache.org (BP build)")]
 [assembly: AssemblyProduct("ZooKeeperNet")]
 [assembly: AssemblyCopyright("Copyright 2007-2012 ewhauser, omwok. - All rights reserved.")]
-[assembly: AssemblyVersion("3.3.4.8")]
-[assembly: AssemblyFileVersion("3.3.4.8")]
+[assembly: AssemblyVersion("3.4.6.2")]
+[assembly: AssemblyFileVersion("3.4.6.2")]
 
-[assembly: AssemblyInformationalVersion("3.3.4.8")]
+[assembly: AssemblyInformationalVersion("3.4.6.2")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
 
